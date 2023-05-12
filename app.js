32f50ff0de81ced93826a56d661c042690c602be
lvl mja <84472102+Eve-SatOrU@users.noreply.github.com>
2023-05-12 16:43:54 +0100

    baka

--- app.js
+++ app.js
@@ -8,14 +8,11 @@ app.set('views', 'views');
 
 app.use(express.static('public'));
 
-//admin routes
 const routes = require('./routes/routes');
 app.use('/', routes);
 
 const userController = require('./controllers/usercontroller');
 const errorController = require('./controllers/error.js');
- //error
-// app.use(errorController.get404);
 
   app.listen(3000, () => {
     console.log('Server started on port 3000');
